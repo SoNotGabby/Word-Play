1f8b35e794ce28d1b4c9f38e19d0fb741be14b9a
Gabby <gabriellelaah@gmail.com>
2025-10-14 10:52:29 -0500

    Initial Commit

---
+++ Word Play/Word_PlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_PlayApp.swift
+//  Word Play
+//
+//  Created by gabrielle on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Word_PlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
