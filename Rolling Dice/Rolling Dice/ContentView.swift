22f11711cd5982e12c6945ee17e635d718030126
Gabby <gabriellelaah@gmail.com>
2025-10-20 10:41:42 -0500

    Created random die values

---
+++ Rolling Dice/Rolling Dice/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Rolling Dice
+//
+//  Created by gabrielle on 10/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var randomValue = 0
+    var body: some View {
+        VStack {
+        Text("Dice Roll")
+                .font(.title)
+            Text("\(randomValue)")
+                .font(.system(size: 72))
+                .padding()
+                .onTapGesture {
+                    randomValue = Int.random(in: 1...6)
+                }
+            Spacer()
+                
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rolling Dice/Rolling Dice/Rolling_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rolling_DiceApp.swift
+//  Rolling Dice
+//
+//  Created by gabrielle on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Rolling_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
